744a8c1f26a5bb34c2eef593056ca4ee49e65e5c
izaleski <iwo.zaleski@payback.net>
2024-07-11 13:10:46 +0200

    Changed main module because the dynamic block in google_project_iam_member is incorrect because google_project_iam_member doesn't support nested blocks.

--- main.tf
+++ main.tf
@@ -11,22 +11,30 @@ resource "google_service_account" "service_accounts" {
 }
 
 resource "google_project_iam_member" "service_account_roles" {
-  for_each = { for k, v in var.service_accounts : k => v.roles }
+  for_each = { for sa, details in var.service_accounts : sa => details.roles }
 
   project = var.project_id
-  role    = each.value[0]
-  member  = "serviceAccount:${google_service_account.service_accounts[each.key].email}"
 
-  lifecycle {
-    ignore_changes = [role] # Prevents Terraform from recreating the resource when roles change
-  }
+  # This creates one iam_member for each role
+  member = "serviceAccount:${google_service_account.service_accounts[each.key].email}"
+  role   = each.value[0] # We will loop through all roles later
+}
 
-  dynamic "roles" {
-    for_each = range(length(each.value))
-
-    content {
-      role   = each.value[roles.value]
-      member = "serviceAccount:${google_service_account.service_accounts[each.key].email}"
+resource "google_project_iam_member" "service_account_role" {
+  for_each = {
+    for sa, details in var.service_accounts : sa => {
+      for role in details.roles : "${sa}-${role}" => {
+        service_account = sa
+        role            = role
+      }
     }
   }
+
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.service_accounts[each.value.service_account].email}"
+  role    = each.value.role
+}
+
+output "service_account_emails" {
+  value = { for sa in google_service_account.service_accounts : sa.key => sa.value.email }
 }
